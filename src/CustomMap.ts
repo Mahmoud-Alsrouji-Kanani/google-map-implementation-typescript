647680eaa9cf02575589e7bbb5e03fb759d94a3f
Mahmoud Kanani <mahmoud.kanani@damacgroup.com>
2020-07-10 20:43:48 +0400

    Adding the markers the best way with an Interface to set some instructions

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,16 @@
-import { User } from './User'; // to refer to the user type and use it for annotation
-import { Company } from './Company';
+//import { User } from './User'; // to refer to the user type and use it for annotation
+//import { Company } from './Company';
+
+
+// we will some instructions on how an object can be an argument to the addMarker function
+// we will do that by creating an interface
+// so a user, company or an object from any class must satisfy that interface 'Mappable' to work with the CustomMap's addMarker method
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  }
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map; // we can not reference this property from outside this class
@@ -11,7 +22,7 @@ export class CustomMap {
     });
   }
 
-  addMarker(mappable: User | Company): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
@@ -20,14 +31,4 @@ export class CustomMap {
       }
     });
   }
-
-  // addCompanyMarker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: company.location.lat,
-  //       lng: company.location.lng
-  //     }
-  //   });
-  // }
 }
