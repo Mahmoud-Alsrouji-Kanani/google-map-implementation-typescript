348c6dbbe481e917b11ddb95e31a84a1d4837157
Mahmoud Kanani <mahmoud.kanani@damacgroup.com>
2020-07-10 20:48:24 +0400

    Adding the markers the best way with an Interface to set some instructions

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,10 +1,7 @@
-//import { User } from './User'; // to refer to the user type and use it for annotation
-//import { Company } from './Company';
-
-
 // we will some instructions on how an object can be an argument to the addMarker function
 // we will do that by creating an interface
 // so a user, company or an object from any class must satisfy that interface 'Mappable' to work with the CustomMap's addMarker method
+// we call this an Implicit check, TypeScript (implicitly) behind the scenes is making sure that the user, company has the correct properties with the correct types inside of it to be an argument for addMarker
 interface Mappable {
   location: {
     lat: number;
